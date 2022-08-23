ab1a2822bc5cc2a9e2b0e80609c1a75dd2a5ec25
dacosta <dacsolo10@gmail.com>
2022-08-23 18:27:02 +0000

    write to file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - create a file.
+ * @filename: filename.
+ * @text_content: content to write in the file.
+ *
+ * Return: 1 if success. -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int ptr;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+	ptr = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (ptr == -1)
+		return (-1);
+	if (!text_content)
+		text_content = " ";
+	nletters = strlen(text_content);
+	rwr = write(ptr, text_content, nletters);
+	if (rwr == -1)
+		return (-1);
+	close(ptr);
+
+	return (1);
+}
