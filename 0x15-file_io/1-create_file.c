a0e3b1c69c2a8f708179ce7323acab1997021a09
dacosta <dacsolo10@gmail.com>
2022-08-23 18:30:52 +0000

    write to file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (!text_content)
 		text_content = " ";
-	nletters = strlen(text_content);
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
 	rwr = write(ptr, text_content, nletters);
 	if (rwr == -1)
 		return (-1);
